37667427614a6ec7f7dd2a5ae46c022a78fd63e2
Stefan Gisi <141892560+stgisi414@users.noreply.github.com>
2025-06-30 09:50:45 +0000

    Change copyright name

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ export const Footer: React.FC<FooterProps> = ({ className = '' }) => {
     return (
         <footer className={`bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 py-4 px-6 text-center ${className}`}>
             <div className="text-sm text-gray-600 dark:text-gray-400">
-                © 2025 <span className="font-semibold text-gray-900 dark:text-white signatex-embossed">Signatex</span>. All rights reserved.
+                © 2025 <span className="font-semibold text-gray-900 dark:text-white signatex-embossed">Signatex.co</span>. All rights reserved.
             </div>
         </footer>
     );
